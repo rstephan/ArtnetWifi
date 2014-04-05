87e3cf242addd05bf1b74b5ed9ac8274b54bb3ce
Nathanaël Lécaudé <nat@studioimaginaire.com>
2014-04-05 19:35:52 -0400

    Comments

--- Artnet.h
+++ Artnet.h
@@ -5,13 +5,16 @@
 #include <Ethernet.h>
 #include <EthernetUdp.h>
 
+// UDP specific
+#define ART_NET_PORT 6454
+// Opcodes
 #define ART_POLL 0x2000
 #define ART_DMX 0x5000
+// Buffers
 #define MAX_BUFFER_ARTNET 530
-#define ART_DMX_START 18
+// Packet
 #define ART_NET_ID "Art-Net\0"
-#define ART_NET_PORT 6454
-
+#define ART_DMX_START 18
 
 class Artnet
 {
