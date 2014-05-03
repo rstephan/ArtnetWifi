b04b20c20017307c28f02bfe1404b1d085c0da7d
Nathanaël Lécaudé <nat@studioimaginaire.com>
2014-05-03 19:59:44 -0400

    Added NeoPixel multiple unviverses example

---
+++ examples/ArtnetNeopixel/ArtnetNeopixel.ino
@@ -0,0 +1,86 @@
+#include <Artnet.h>
+#include <Ethernet.h>
+#include <EthernetUdp.h>
+#include <SPI.h>
+#include <Adafruit_NeoPixel.h>
+
+// Neopixel settings
+const int ledsPerStrip = 492; // change for your setup
+const byte dataPin = 2;
+Adafruit_NeoPixel leds = Adafruit_NeoPixel(ledsPerStrip, dataPin, NEO_GRB + NEO_KHZ800);
+
+// Artnet settings
+Artnet artnet;
+const int startUniverse = 0; // CHANGE FOR YOUR SETUP most software this is 1, some software send out artnet first universe as zero.
+const int numberOfChannels = ledsPerStrip * 3; // Total number of channels you want to receive (1 led = 3 channels)
+byte channelBuffer[numberOfChannels]; // Combined universes into a single array
+
+// Check if we got all universes
+const int maxUniverses = ceil(numberOfChannels / 512.);
+bool universesReceived[maxUniverses];
+bool sendFrame = 1;
+
+// Change ip and mac address for your setup
+byte ip[] = {192, 168, 2, 2};
+byte mac[] = {0x04, 0xE9, 0xE5, 0x00, 0x69, 0xEC};
+
+void setup()
+{
+  Serial.begin(115200);
+  artnet.begin(mac, ip);
+  leds.begin();
+
+  // this will be called for each packet received
+  artnet.setArtDmxCallback(onDmxFrame);
+}
+
+void loop()
+{
+  // we call the read function inside the loop
+  artnet.read();
+}
+
+void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* data)
+{
+  // set brightness of the whole strip 
+  if (universe == 15)
+  {
+    leds.setBrightness(data[0]);
+  }
+
+  // Store which universe has got in
+  if (universe < maxUniverses)
+    universesReceived[universe] = 1;
+
+  for (int i = 0 ; i < maxUniverses ; i++)
+  {
+    if (universesReceived[i] == 0)
+    {
+      //Serial.println("Broke");
+      sendFrame = 0;
+      break;
+    }
+  }
+
+  // read universe and put into the right part of the display buffer
+  for (int i = 0 ; i < length ; i++)
+  {
+    int bufferIndex = i + ((universe - startUniverse) * length);
+    if (bufferIndex < numberOfChannels) // to verify
+      channelBuffer[bufferIndex] = byte(data[i]);
+  }      
+
+  // send to leds
+  for (int i = 0; i < ledsPerStrip; i++)
+  {
+    leds.setPixelColor(i, channelBuffer[(i) * 3], channelBuffer[(i * 3) + 1], channelBuffer[(i * 3) + 2]);
+  }      
+  
+  if (sendFrame)
+  {
+    leds.show();
+    // Reset universeReceived to 0
+    memset(universesReceived, 0, maxUniverses);
+    sendFrame = 1;
+  }
+}
